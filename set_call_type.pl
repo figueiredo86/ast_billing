3ae1b4b87c880d307b9b0347b8c522fcca0bdbc5
root <root@host03.(none)>
2014-02-20 16:07:16 +0100

    Script para setar o tipo da chamada realizada

--- set_call_type.pl
+++ set_call_type.pl
@@ -1,1 +1,51 @@
-/usr/share/asterisk/agi-bin/set_call_type.pl
+#!/usr/bin/perl -w
+
+use Asterisk::AGI;
+
+$agi = new Asterisk::AGI;
+
+sub main () {
+ # recebe e define as variáveis que serão utilizadas na sessão
+ my $exten = $ARGV[0];
+ &check_ext_type($exten);
+ &select_route($call_type);
+}
+
+sub check_ext_type () {
+ my $exten = $_[0];
+ if (substr($exten,0,1) != 0) {
+  $agi->noop("essa é uma chamada interna e não será tarifada");
+  $agi->set_variable("CDR(userfield)","interna");
+  $call_type = "interna";
+  return $call_type;
+ } 
+ elsif (substr($exten,1,1) != 0) {
+  $agi->noop("essa é uma chamada local e o minuto é igual a 0.05");
+  $agi->set_variable("CDR(userfield)","local");
+  $call_type = "local";
+  return $call_type;
+ }
+ elsif (substr($exten,1,1) == 0) {
+  if (substr($exten,2,1) == 0) {
+   $agi->noop("essa é uma chamada internacional e o minuto é igual a 1.30");
+   $agi->set_variable("CDR(userfield)","ddi");
+   $call_type = "ddi";
+   return $call_type;
+  }
+  else { 
+   $agi->noop("essa é uma chamada ddd e o minuto é igual a 0.15");
+   $agi->set_variable("CDR(userfield)","ddd");
+   $call_type = "ddd";
+   return $call_type;
+  }
+ }
+}
+
+
+sub select_route () {
+ my $call_type = $_[0];
+ $agi->noop($call_type);
+}
+
+&main;
+exit 0;
